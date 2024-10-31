61c3faa81fb666692c1e89de5b9b74f41e0f6742
Dann Johnrem A. De Guzman <jonrhem10@gmail.com>
2024-11-01 02:27:40 +0800

    report page make responsive, WIP for welcome page

--- resources/views/pages/attendance/report.blade.php
+++ resources/views/pages/attendance/report.blade.php
@@ -4,24 +4,24 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <h1 class="text-2xl font-bold mb-4">Generate Attendance Report by Date Range</h1>
+                    <h1 class="mb-4 text-2xl font-bold">Generate Attendance Report by Date Range</h1>
 
                     <form method="GET" action="{{ route('attendance.report.range') }}">
                         @csrf
-                        <div class="grid grid-cols-2 gap-6 mb-4">
+                        <div class="grid gap-6 mb-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
                             <div>
                                 <label for="start_date" class="block text-sm font-medium text-gray-700">Start Date</label>
-                                <input type="date" name="start_date" id="start_date" class="mt-1 block w-full border-gray-300 shadow-sm sm:text-sm rounded-md" required>
+                                <input type="date" name="start_date" id="start_date" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm sm:text-sm" required>
                             </div>
                             <div>
                                 <label for="end_date" class="block text-sm font-medium text-gray-700">End Date</label>
-                                <input type="date" name="end_date" id="end_date" class="mt-1 block w-full border-gray-300 shadow-sm sm:text-sm rounded-md" required>
+                                <input type="date" name="end_date" id="end_date" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm sm:text-sm" required>
                             </div>
                         </div>
-                        <button type="submit" class="px-4 py-2 bg-blue-500 text-white font-bold rounded">Generate Report</button>
+                        <button type="submit" class="px-4 py-2 font-bold text-white bg-blue-500 rounded">Generate Report</button>
                     </form>
                 </div>
             </div>
